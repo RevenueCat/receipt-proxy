b96019d59e29787fb3dde6a81e0311b82258f18d
Jacob Eiting <jacob.eiting@gmail.com>
2017-11-09 21:31:31 -0800

    CORS support.

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ var server = http.createServer(function(req, res) {
   var target = (req.url !== "/sandbox") ? "https://buy.itunes.apple.com/verifyReceipt" 
                                         : "https://sandbox.itunes.apple.com/verifyReceipt";
   proxy.web(req, res, { target: target, ignorePath: true });
+
+  res.setHeader('Access-Control-Allow-Origin', 'www.revenuecat.com');
+  res.setHeader('Access-Control-Request-Method', 'www.revenuecat.com');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', 'www.revenuecat.com');
+
+  if ( req.method === 'OPTIONS' ) {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
 });
 
 server.listen(process.env.PORT);
