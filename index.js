c74089d4f8a4335361f3b4052c4b969ad4d58fce
Jacob Eiting <jacob.eiting@gmail.com>
2017-11-09 21:25:58 -0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var httpProxy = require('http-proxy');
+
+var proxy = httpProxy.createProxyServer({
+  secure: false
+});
+
+var server = http.createServer(function(req, res) {
+  var target = (req.url !== "/sandbox") ? "https://buy.itunes.apple.com/verifyReceipt" 
+                                        : "https://sandbox.itunes.apple.com/verifyReceipt";
+  proxy.web(req, res, { target: target, ignorePath: true });
+});
+
+server.listen(5050);
