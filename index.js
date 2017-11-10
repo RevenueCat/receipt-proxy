d432e80fe9ee14acfb6596e88a56676de53bf166
Jacob Eiting <jacob.eiting@gmail.com>
2017-11-09 21:34:47 -0800

    Fix CORS.

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ var server = http.createServer(function(req, res) {
   proxy.web(req, res, { target: target, ignorePath: true });
 
   res.setHeader('Access-Control-Allow-Origin', 'www.revenuecat.com');
-  res.setHeader('Access-Control-Request-Method', 'www.revenuecat.com');
+  res.setHeader('Access-Control-Request-Method', '*');
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-  res.setHeader('Access-Control-Allow-Headers', 'www.revenuecat.com');
+  res.setHeader('Access-Control-Allow-Headers', '*');
 
   if ( req.method === 'OPTIONS' ) {
     res.writeHead(200);
